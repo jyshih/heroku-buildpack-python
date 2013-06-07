954d10fef1a73ce6731bd288e2b34322df7a41af
jyshih <jonathanyshih@gmail.com>
2013-06-07 11:08:27 -0700

    Fixing the check for a cached installation of glpk

--- vendor/cvxopt/src/setup.py
+++ vendor/cvxopt/src/setup.py
@@ -36,10 +36,10 @@ FFTW_INC_DIR = '/usr/include'
 BUILD_GLPK = 1 
 
 # Directory containing libglpk (used only when BUILD_GLPK = 1).
-GLPK_LIB_DIR = '/app/.heroku/vendor/lib'
+GLPK_LIB_DIR = '/app/.heroku/vendor/glpk/lib'
 
 # Directory containing glpk.h (used only when BUILD_GLPK = 1).
-GLPK_INC_DIR = '/app/.heroku/vendor/include'
+GLPK_INC_DIR = '/app/.heroku/vendor/glpk/include'
 
 # Set to 1 if you are installing the DSDP module.
 BUILD_DSDP = 0
